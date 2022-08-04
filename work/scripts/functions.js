40eb13616e4a71029e0fc8fede982882c602e075
ericcouture <63058212+ericcouture@users.noreply.github.com>
2022-08-04 19:20:12 -0400

    upload images add work pages

--- work/scripts/functions.js
+++ work/scripts/functions.js
@@ -1,13 +1,46 @@
 function toggleWidth() {
     var right = document.getElementById("right");
     var left = document.getElementById("left");
-    right.classList.toggle("right-fullwidth");
+    right.classList.add("right-fullwidth");
     // right.classList.toggle("right-fullwidth.background-image");
-    left.classList.toggle("left-fullwidth");
+    left.classList.add("left-fullwidth");
 }
 
 function worlds() {
     var frame = document.getElementById("frame");
     frame.src = "pieces/worlds.html";
-    frame.classList.toggle("hidden")
+    if (frame.classList.contains("hidden")) {
+        frame.src = "pieces/worlds.html";
+        frame.classList.toggle("hidden");
+    }
+    else {
+        frame.src = "pieces/worlds.html";
+    }
+
+}
+
+function worldsBook() {
+    var frame = document.getElementById("frame");
+
+    if (frame.classList.contains("hidden")) {
+        frame.src = "pieces/worlds-book.html";
+        frame.classList.toggle("hidden");
+    }
+    else {
+        frame.src = "pieces/worlds-book.html";
+    }
+
+}
+
+function queerJoy() {
+    var frame = document.getElementById("frame");
+
+    if (frame.classList.contains("hidden")) {
+        frame.src = "pieces/queer-joy.html";
+        frame.classList.toggle("hidden");
+    }
+    else {
+        frame.src = "pieces/queer-joy.html";
+    }
+
 }
